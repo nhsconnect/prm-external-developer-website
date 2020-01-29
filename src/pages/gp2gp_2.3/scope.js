b1f4d7f3f7329e4119f2bf9b7e22d9cbe46b2458
jdunleav <jdunleav@thoughtworks.com>
2020-01-29 09:27:48 +0000

    Updates to page styling to improve readability

--- src/pages/gp2gp_2.3/scope.js
+++ src/pages/gp2gp_2.3/scope.js
@@ -13,18 +13,23 @@ const Layout = ({ children }) => (
       }}
     />
     <div className="nhsuk-width-container ">
-      <main className="nhsuk-main-wrapper main-content ">
-        <PermenantMenu />
-        <article>
-          <h2>GP2GP 2.3: Enhanced Managment Information (MI)</h2>
-          <a name="scope"></a>
-          <h3>Scope</h3>
-          <p>
-            When a sending system generates an EHR extract, the following JSON
-            MI event must be submitted. In the first instance we are interested
-            in capturing attachment metadata.
-          </p>
-          <pre>{`
+      <main className="nhsuk-main-wrapper">
+        <div className="nhsuk-grid-row">
+          <div className="nhsuk-grid-column-full">
+            <div className="app-pane">
+              <div className="app-pane__side-bar">
+                <PermenantMenu />
+              </div>
+              <div className="app-pane__main-content">
+                <h1>GP2GP 2.3: Enhanced Managment Information (MI)</h1>
+                <a name="scope"></a>
+                <h2>Scope</h2>
+                <p>
+                  When a sending system generates an EHR extract, the following
+                  JSON MI event must be submitted. In the first instance we are
+                  interested in capturing attachment metadata.
+                </p>
+                <pre>{`
 {
   "id": "ABC123",
   "event_type": "ehr_generated",
@@ -37,71 +42,85 @@ const Layout = ({ children }) => (
 }
           `}</pre>
 
-          <h4>Event fields</h4>
-          <table>
-            <tr>
-              <th>Field</th>
-              <th>Description</th>
-            </tr>
-            <tr>
-              <td>id</td>
-              <td>Unique identifier for this event.</td>
-            </tr>
-            <tr>
-              <td>event_type</td>
-              <td>The type of this event: “ehr_generated”</td>
-            </tr>
-            <tr>
-              <td>registration_id</td>
-              <td>
-                Unique identifier of the registration this event belongs to
-              </td>
-            </tr>
-            <tr>
-              <td>timestamp</td>
-              <td>Unix epoch timestamp of when the EHR was generated</td>
-            </tr>
-            <tr>
-              <td>attachment</td>
-              <td>
-                JSON list containing metadata for each attachment in the EHR
-              </td>
-            </tr>
-          </table>
+                <h3>Event fields</h3>
+                <table>
+                  <tbody>
+                    <tr>
+                      <th>Field</th>
+                      <th>Description</th>
+                    </tr>
+                    <tr>
+                      <td>id</td>
+                      <td>Unique identifier for this event.</td>
+                    </tr>
+                    <tr>
+                      <td>event_type</td>
+                      <td>The type of this event: “ehr_generated”</td>
+                    </tr>
+                    <tr>
+                      <td>registration_id</td>
+                      <td>
+                        Unique identifier of the registration this event belongs
+                        to
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>timestamp</td>
+                      <td>
+                        Unix epoch timestamp of when the EHR was generated
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>attachment</td>
+                      <td>
+                        JSON list containing metadata for each attachment in the
+                        EHR
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
 
-          <h4>Attachment metadata fields</h4>
-          <table>
-            <tr>
-              <th>Field</th>
-              <th>Description</th>
-            </tr>
-            <tr>
-              <td>id</td>
-              <td>
-                The ID of the attachment (as found in the XML of the EHR
-                extract)
-              </td>
-            </tr>
-            <tr>
-              <td>mimetype</td>
-              <td>The mime type of the attachment</td>
-            </tr>
-            <tr>
-              <td>size_bytes</td>
-              <td>The size in bytes of the attachment</td>
-            </tr>
-            <tr>
-              <td>timestamp</td>
-              <td>Unix epoch timestamp of when the EHR was generated</td>
-            </tr>
-            <tr>
-              <td>attachment</td>
-              <td>
-                JSON list containing metadata for each attachment in the EHR
-              </td>
-            </tr>
-          </table>
-        </article>
+                <h3>Attachment metadata fields</h3>
+                <table>
+                  <tbody>
+                    <tr>
+                      <th>Field</th>
+                      <th>Description</th>
+                    </tr>
+                    <tr>
+                      <td>id</td>
+                      <td>
+                        The ID of the attachment (as found in the XML of the EHR
+                        extract)
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>mimetype</td>
+                      <td>The mime type of the attachment</td>
+                    </tr>
+                    <tr>
+                      <td>size_bytes</td>
+                      <td>The size in bytes of the attachment</td>
+                    </tr>
+                    <tr>
+                      <td>timestamp</td>
+                      <td>
+                        Unix epoch timestamp of when the EHR was generated
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>attachment</td>
+                      <td>
+                        JSON list containing metadata for each attachment in the
+                        EHR
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   </React.Fragment>
@@ -109,29 +128,35 @@ const Layout = ({ children }) => (
 
 const PermenantMenu = ({ children }) => (
   <nav
-    className="nhsuk-contents-list permenant-menu"
-    role="navigation"
-    aria-label="Pages in this guide"
-  >
-    <h2 className="nhsuk-u-visually-hidden">Contents</h2>
-    <ol className="nhsuk-contents-list__list">
-      <li className="nhsuk-contents-list__item">
-        <a className="nhsuk-contents-list__link" href="/prm-external-developer-website/gp2gp_2.3">
-          Overview
-        </a>
-      </li>
-      <li className="nhsuk-contents-list__item">
-        <a className="nhsuk-contents-list__link" href="/prm-external-developer-website/gp2gp_2.3/vision">
-          Vision
-        </a>
-      </li>
-      <li className="nhsuk-contents-list__item">
-        <a className="nhsuk-contents-list__link" href="#scope">
-          Scope
-        </a>
-      </li>
-    </ol>
-  </nav>
+  className="nhsuk-contents-list permenant-menu"
+  role="navigation"
+  aria-label="Pages in this guide"
+>
+  <h2 className="nhsuk-u-visually-hidden">Contents</h2>
+  <ol className="nhsuk-list">
+    <li className="app-side-nav__item">
+      <a className="app-side-nav__link" href="#overview">
+        Overview
+      </a>
+    </li>
+    <li className="app-side-nav__item">
+      <a
+        className="app-side-nav__link"
+        href="/prm-external-developer-website/gp2gp_2.3/vision"
+      >
+        Vision
+      </a>
+    </li>
+    <li className="app-side-nav__item app-side-nav__item--current">
+      <a
+        className="app-side-nav__link"
+        href="/prm-external-developer-website/gp2gp_2.3/scope"
+      >
+        Scope
+      </a>
+    </li>
+  </ol>
+</nav>
 )
 
 export default Layout
