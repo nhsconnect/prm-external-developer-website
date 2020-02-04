e7e4092e6e4155ccd0e1d38258288b40e82c2ce4
jdunleav <jdunleav@thoughtworks.com>
2020-02-04 09:20:21 +0000

    Index page refactored to new look and feel

--- src/components/pagelayouts/readingpage.js
+++ src/components/pagelayouts/readingpage.js
@@ -3,7 +3,7 @@ import React from "react"
 import { Layout } from "../layout"
 import { SharedHeader } from "../sharedheader"
 
-export const PageWithSideMenu = function({ children }) {
+export const ReadingPage = function({ children }) {
   return (
     <React.Fragment>
       <SharedHeader />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,15 @@
 import React from "react"
-import Header from "../components/header"
-import { Layout } from "../components/layout"
-import { PermenantMenu } from "../components/permenantmenu"
+import { ReadingPage } from "../components/pagelayouts/readingpage"
 import "./index.scss"
 
 const Page = ({ children }) => (
-  <React.Fragment>
-    <Header
-      serviceName={"Patient Record Migration: Developer website"}
-      phaseBanner={{
-        text:
-          "This website and the standards contained is in early development. Whilst we hope you find it useful we would not recommend using this as your only source of guidance for developing.",
-        tag: "Development:",
-      }}
-    />
-    <Layout>
-      <div className="app-pane__side-bar">
-        <PermenantMenu
-          data={{
-            items: [
-              {
-                url: "/prm-external-developer-website/gp2gp",
-                label: "GP2GP",
-              },
-            ],
-          }}
-        />
-      </div>
-      <div className="app-pane__main-content">
-        <h1>Patient Record Migration: Developer website</h1>
-        <p>
-          Welcome to the Patient Record Migration: Developer website. Please
-          click a link to the area you are interested in.
-        </p>
-      </div>
-    </Layout>
-  </React.Fragment>
+  <ReadingPage>
+    <h1>Patient Record Migration: Developer website</h1>
+    <p>
+      Welcome to the Patient Record Migration: Developer website. Please click a
+      link to the area you are interested in.
+    </p>
+  </ReadingPage>
 )
 
 export default Page
